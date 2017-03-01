1ccbe146d263d44e77953dc03a8394dcd2ef42f7
Aaron Frase <afrase91@gmail.com>
2017-03-01 06:56:47 -0500

    Starting work on parser and AST
    Beginning to create the parser and the AST with tests.
    Also created a test for token.

---
+++ token/token_test.go
@@ -0,0 +1,29 @@
+package token
+
+import (
+	"testing"
+)
+
+func TestLookupIdent(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected TokenType
+	}{
+		{input: "let", expected: LET},
+		{input: "fn", expected: FUNCTION},
+		{input: "true", expected: TRUE},
+		{input: "if", expected: IF},
+		{input: "false", expected: FALSE},
+		{input: "else", expected: ELSE},
+		{input: "return", expected: RETURN},
+		{input: "fooBar", expected: IDENT},
+	}
+
+	for i, tt := range tests {
+		tok := LookupIdent(tt.input)
+
+		if tok != tt.expected {
+			t.Fatalf("tests[%d] - tokenType wrong. expected=%q, got=%q", i, tt.expected, tok)
+		}
+	}
+}
